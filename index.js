ef147e7ab32bd3210ca5b0b70bcb5b9628bbcc20
Jackson Eckert <jaxon.eckert@gmail.com>
2024-12-16 18:20:12 -0600

    Add main die roll logic and animation

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
-console.log("Hello world!");
-// TODO: wire up the die roll click event
+die = document.querySelector("#die");
+resultDisplay = document.querySelector(".roll-result");
+
+handleDieRoll = () => {
+  resultDisplay.style.display = "block";
+  resultDisplay.textContent = "Thinking...";
+  die.classList.add("roll-animation");
+
+  setTimeout(() => {
+    sideInput = document.querySelector("#numSides");
+    numSides = sideInput.value;
+    console.log(`Sides on die: ${numSides}`);
+    result = Math.floor(Math.random() * numSides) + 1;
+    die.classList.remove("roll-animation");
+    resultDisplay.textContent = `You rolled ${result}!`;
+  }, 1000);
+};
+
+die.addEventListener("click", handleDieRoll);
